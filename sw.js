5321260a40faa4e002e14989aa36af89a821c790
Aleks Popovic <aleksandar88popovic@gmail.com>
2021-01-11 19:52:10 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,23 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b1839fb168449786dd41.js"
+    "url": "webpack-runtime-3ebcecfd8a30be9834cd.js"
   },
   {
-    "url": "styles.455f441885e496018bc5.css"
+    "url": "styles.334c1fdb50399a26e1e1.css"
   },
   {
     "url": "styles-8636a280cbc61d53ad10.js"
   },
   {
-    "url": "framework-37df0b5633b2d42f8bc2.js"
+    "url": "framework-75a3c0adc3625ac15341.js"
   },
   {
-    "url": "app-55e06a72731649180d28.js"
+    "url": "app-6b1c1757f46899607023.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "21e5f6fe2ab41916c573b1aa0a4bd10b"
+    "revision": "5abc42fa88afea88c3a15400808b8259"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-cc2751a409622c7dca7b.js"
@@ -54,16 +54,16 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "7b9a85c95d133d4db3d8b7647d9bde82"
+    "revision": "db5253f28a77d8e6e0e6a071c877a995"
   },
   {
     "url": "a9a7754c-8dc4b8ed258b76827098.js"
   },
   {
-    "url": "14bfd844193e4fb3cbfc42acca541fc203ab5bad-c6da8bb5b0322cc53c93.js"
+    "url": "74e6cb5473ae529447f715d6a1c8eea99ca919a1-e043704146ffd84c87d5.js"
   },
   {
-    "url": "component---src-pages-index-js-971a1ac9684b909f39b5.js"
+    "url": "component---src-pages-index-js-f4413250c3239b47752a.js"
   },
   {
     "url": "page-data/index/page-data.json",
@@ -111,21 +111,21 @@ self.__precacheManifest = [
     "revision": "1b56bf425ea61fd6e12c419c052d296c"
   },
   {
-    "url": "component---src-pages-tourist-guide-js-481c7364ac242b951674.js"
+    "url": "component---src-pages-tourist-guide-js-386f2e46692a2cd60eee.js"
   },
   {
     "url": "page-data/touristGuide/page-data.json",
     "revision": "fa74aa98edbf284b64cdb3f9eed1b89a"
   },
   {
-    "url": "component---src-pages-travel-info-belgrade-js-931f3791629bd8ec08a3.js"
+    "url": "component---src-pages-travel-info-belgrade-js-bfb9043e97742f7cc855.js"
   },
   {
     "url": "page-data/travelInfoBelgrade/page-data.json",
     "revision": "0102d20321bdd6a1e95e9713394d49fc"
   },
   {
-    "url": "component---src-pages-travel-info-faculty-js-21f5edf17596d3ad0621.js"
+    "url": "component---src-pages-travel-info-faculty-js-7595721074437e25c3b1.js"
   },
   {
     "url": "page-data/travelInfoFaculty/page-data.json",
@@ -226,7 +226,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/icgl15-test/app-55e06a72731649180d28.js`))) {
+  if (!resources || !(await caches.match(`/icgl15-test/app-6b1c1757f46899607023.js`))) {
     return await fetch(event.request)
   }
 
